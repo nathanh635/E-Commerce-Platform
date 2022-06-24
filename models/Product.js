e96349f6ac0d9bfe5db5e8479d6535f2b3da9a01
nathanh635 <nathanh635@gmail.com>
2022-06-23 20:24:13 -0600

    updated models

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,41 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        //validates that the value is a decimal
+        isDecimal: true,
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+      //validates that the value is numeric
+      isNumeric: true,
+    }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+          references: {
+          model: 'category',
+          key: 'id',
+        },
+      },
   },
   {
     sequelize,
